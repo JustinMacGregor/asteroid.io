3b517bc7702cc08c035a02a30fc23968eaef1528
Justin MacGregor <macgreju@sheridancollege.ca>
2020-04-06 18:16:13 -0400

    Miscellaneous Google Authentication fixes

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -15,11 +15,20 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "window.plugins.googleplus"
       ]
+    },
+    {
+      "id": "cordova-plugin-ionic-webview.IonicWebView",
+      "file": "plugins/cordova-plugin-ionic-webview/src/www/util.js",
+      "pluginId": "cordova-plugin-ionic-webview",
+      "clobbers": [
+        "Ionic.WebView"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-whitelist": "1.3.4",
     "cordova-plugin-device": "2.0.3",
-    "cordova-plugin-googleplus": "8.4.0"
+    "cordova-plugin-googleplus": "8.4.0",
+    "cordova-plugin-ionic-webview": "4.1.3"
   };
 });
